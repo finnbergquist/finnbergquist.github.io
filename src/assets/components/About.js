5a47b3f216fb912683cd50f96ecfd121c982ddcd
finnbergquist <fbergqui@bowdoin.edu>
2021-07-28 13:45:16 -0400

    styled photos

--- src/assets/components/About.js
+++ src/assets/components/About.js
@@ -6,7 +6,7 @@ import NavBar from "../containers/NavBar";
 import SocialLinks from "./SocialLinks";
 
 // Image
-import logo from "../images/logo.svg";
+import finn_photo from "../images/Finn_photo.png"
 
 const About = ({ theme, setTheme, githubUrl, name, link, bio, twitter }) => {
   const newTheme = `${theme} d-flex flex-column min-vh-100 justify-content-center`;
@@ -22,13 +22,11 @@ const About = ({ theme, setTheme, githubUrl, name, link, bio, twitter }) => {
       <NavBar theme={theme} setTheme={setTheme} />
       <div className="container text-center">
         <img
-          className="logo spin img-fluid"
-          src={logo}
-          alt="React Logo"
-          height="45%"
-          width="45%"
+          src={finn_photo}
+          alt="finn_phto"
+          style={{width: 300, height: 400, float: 'left'}}
         />
-        <h1>{name}</h1>
+        <h1>{"Finn Bergquist"}</h1>
         <hr />
         <p>{bio}</p>
         <SocialLinks {...socialData} />
